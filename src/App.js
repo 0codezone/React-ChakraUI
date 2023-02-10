e2bc51042b6092c1ce95840e4373f393cedf4941
0codezone <mohittailor997@gmail.com>
2023-02-10 18:28:35 +0530

    second commit , Add home page and work in it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
